92c7166f04e0139c4148e0b52a521ae8866dadb5
z0xm <mukulz2020@gmail.com>
2025-01-02 01:17:33 +0530

    stupid mistake

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -45,7 +45,7 @@ export async function updateSession(request: NextRequest) {
 	if (user && request.nextUrl.pathname.startsWith('/login')) {
 		const url = request.nextUrl.clone();
 		url.pathname = '/';
-		return NextResponse;
+		return NextResponse.redirect(url);
 	}
 
 	// IMPORTANT: You *must* return the supabaseResponse object as it is.
