0a95fcfdc6825f35d1708c6e86301af301d8db14
z0xm <mukulz2020@gmail.com>
2025-01-03 00:39:52 +0530

    about

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { useAuthStore } from '@/store/auth';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
 import { createClient } from '@/lib/supabase/client';
+import Link from 'next/link';
 
 export default function Navbar() {
 	const { user } = useAuthStore();
@@ -13,7 +14,9 @@ export default function Navbar() {
 	if (user) {
 		return (
 			<div className='flex justify-between items-center px-4 py-2 md:px-6 md:py-4 border-b-2 border-green-400'>
-				<span className='md:text-2xl'>TheHandMadeSmiles</span>
+				<Link href='/about' className='md:text-2xl'>
+					TheHandMadeSmiles
+				</Link>
 				<div className='flex gap-2 md:gap-6'>
 					<Button className='md:text-lg rounded-md' onClick={() => supabaseClient.auth.signOut()}>
 						Logout
