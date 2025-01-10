3cdd1a8b2af5dbef67c5ddd32647dea526579775
z0xm <mukulz2020@gmail.com>
2025-01-11 02:20:46 +0530

    disable notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${font_sour.className}`}>
-                <Notifications />
+                {/* <Notifications /> */}
                 <Navbar />
                 <Toaster />
                 {children}
