3c3ffd8884fd6e5fd9617f0f1a58c4ed20424a57
z0xm <mukulz2020@gmail.com>
2025-01-18 19:05:19 +0530

    feedback page

---
+++ src/app/feedback/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function Feedback() {
+    redirect('https://forms.gle/rUh7TePrr5E8w15E6');
+}
--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -2,6 +2,10 @@ import { createServerClient } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
 
 export async function updateSession(request: NextRequest) {
+    if (request.nextUrl.pathname.startsWith('/feedback')) {
+        return NextResponse.next({ request });
+    }
+
     if (process.env.MAINTENANCE_MODE === 'ON') {
         if (!request.nextUrl.pathname.startsWith('/maintenance')) {
             const url = request.nextUrl.clone();
