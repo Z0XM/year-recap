d336e62656ee1d55ba66a9797305a2c428a41e06
z0xm <mukulz2020@gmail.com>
2025-01-28 12:11:10 +0530

    daily progress

--- src/components/client/DailyFormPage.tsx
+++ src/components/client/DailyFormPage.tsx
@@ -7,6 +7,7 @@ import { Suspense } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card';
 import { Separator } from '../ui/separator';
 import Link from 'next/link';
+import { Progress } from '../ui/progress';
 
 type DailyFormProps =
     | {
@@ -30,7 +31,7 @@ export default function DailyFormPage(props: DailyFormProps) {
         return (
             <div className="flex w-full flex-col items-center justify-center py-6 md:px-10">
                 <div className="mx-4 flex flex-col gap-4 md:w-1/2">
-                    <HelloUser today={today} display_name={user.display_name} />
+                    <HelloUser today={today} display_name={user.display_name} dayInt={dayInt} />
                     <div className="text-xl">
                         <span className="text-primary">Thankyou!</span> You have completed today&apos;s form.
                         <Link href="/p/edit" className="mx-2 text-lg underline duration-200 hover:text-primary">
@@ -46,7 +47,7 @@ export default function DailyFormPage(props: DailyFormProps) {
     return (
         <div className="flex w-full items-center justify-center py-6 md:px-10">
             <div className="mx-4 flex flex-col gap-4 md:w-1/2">
-                <HelloUser today={today} display_name={user.display_name} />
+                <HelloUser today={today} display_name={user.display_name} dayInt={dayInt} />
                 {props.mode === 'edit' && (
                     <div className="text-xl">
                         <span className="text-primary">You are editing!</span> You have completed today&apos;s form.
@@ -58,8 +59,10 @@ export default function DailyFormPage(props: DailyFormProps) {
     );
 }
 
-function HelloUser(props: { today: Date; display_name: string }) {
-    const { today, display_name } = props;
+function HelloUser(props: { today: Date; display_name: string; dayInt: number }) {
+    const { today, display_name, dayInt } = props;
+    const noOfDaysInYear = today.getFullYear() % 4 === 0 ? 366 : 365;
+    const todayCountOfDay = Math.ceil((today.getTime() - new Date(today.getFullYear(), 0, 1).getTime()) / (1000 * 60 * 60 * 24));
     return (
         <>
             <Card className="">
@@ -72,13 +75,12 @@ function HelloUser(props: { today: Date; display_name: string }) {
                         </div>
                     </CardTitle>
                     <CardDescription>
-                        <span className="text-xl text-white">
-                            {Math.ceil((today.getTime() - new Date(today.getFullYear(), 0, 1).getTime()) / (1000 * 60 * 60 * 24))}
-                        </span>
-                        <span className="text-md text-gray-400">{` / ${today.getFullYear() % 4 === 0 ? 366 : 365}`}</span>
+                        <span className="text-xl text-white">{todayCountOfDay}</span>
+                        <span className="text-md text-gray-400">{` / ${noOfDaysInYear}`}</span>
                     </CardDescription>
                 </CardHeader>
-                <Separator className="w-full" />
+                <Progress value={(todayCountOfDay / noOfDaysInYear) * 100} className="w-full" />
+                {/* <Separator className="w-full" /> */}
                 <CardContent className="px-6 py-4">
                     <div className="text-2xl">
                         How was your day <span className={`text-3xl font-bold text-primary`}>{display_name}</span> ?
